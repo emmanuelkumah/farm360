37f7cd5b09377a0b814884c617dee440274594d0
Emmanuel <e.fkumah@gmail.com>
2024-07-04 23:30:40 +0000

    Feat: added navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {} from "react-icons/io";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import avatar from "../data/avatar.jpg";
+import { Notification, UserProfile } from ".";
+import { useStateContext } from "../context/ContextProvider";
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoNotificationsCircleOutline } from "react-icons/io5";
+
+const NavButton = ({ customFunc, icon, color, dotColor }) => (
+  <button
+    type="button"
+    onClick={customFunc}
+    style={{ color }}
+    className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+  >
+    <span
+      style={{ background: dotColor }}
+      className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+    >
+      {icon}
+    </span>
+  </button>
+);
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { activeMenu, setActiveMenu } = useStateContext();
+  return (
+    <div className="flex justify-between p-2 md:mx-6 relative">
+      <NavButton
+        color="blue"
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        icon={<AiOutlineMenu />}
+      />
+      <div className="flex">
+        <NavButton
+          color="blue"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick("notification")}
+          icon={<IoNotificationsCircleOutline />}
+        />
+        <div
+          className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+          onClick={() => handleClick("user profile")}
+        >
+          <img
+            src={avatar}
+            alt="user profile"
+            className="rounded-full w-8 h-8"
+          />
+          <p>
+            <span className="text-gray-400 text-14">Hi, </span>
+            <span className="text-gray-400 font-bold ml-1 text-14">
+              Emmanuel
+            </span>
+          </p>
+          <MdKeyboardArrowDown className="text-gray-400 text-14" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
