bf3eb725b107b4370f43f0b936e62ebbf3679e5e
Emmanuel <e.fkumah@gmail.com>
2024-07-07 22:11:18 +0000

    Modified cards data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,19 +8,33 @@ import { IoQrCodeOutline } from "react-icons/io5";
 const Dashboard = () => {
   return (
     <>
-      <div className="grid gap-5 mt-[30%] mx-10 md:grid-cols-4 md:gap-5 md:m-10">
-        <DashboardCard
-          title="Total Farmers"
-          data="23,445"
-          icon={<FaUserGroup />}
-        />
-        <DashboardCard title="Total Farms" data="34,556" icon={<PiPlant />} />
-        <DashboardCard title="Total Acerage" data="16,703" icon={<LuTrees />} />
-        <DashboardCard
-          title="Total Produce"
-          data="16"
-          icon={<IoQrCodeOutline />}
-        />
+      <div className="mx-10">
+        <div className="grid gap-5 mt-[30%]  md:grid-cols-4 md:m-10">
+          <DashboardCard
+            title="Total Farmers"
+            data="23,445"
+            icon={<FaUserGroup />}
+          />
+          <DashboardCard title="Total Farms" data="34,556" icon={<PiPlant />} />
+          <DashboardCard
+            title="Total Acerage"
+            data="16,703"
+            icon={<LuTrees />}
+          />
+          <DashboardCard
+            title="Total Produce"
+            data="16"
+            icon={<IoQrCodeOutline />}
+          />
+        </div>
+        <div>
+          {/* Line Chart */}
+          <h3>Line Chart here</h3>
+        </div>
+        <div>
+          {/* Activities */}
+          <h3>Activities here</h3>
+        </div>
       </div>
     </>
   );
