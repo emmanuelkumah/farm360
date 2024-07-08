d1a98373928d880ef63a6030618b511fae3d8f8b
Emmanuel <e.fkumah@gmail.com>
2024-07-08 23:58:56 +0000

    feat: Add Farmers list

--- src/pages/Farmers.jsx
+++ src/pages/Farmers.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { Button } from "flowbite-react";
+import { Table } from "flowbite-react";
+import { farmersData } from "../data/demo";
+import { MdDelete, MdEdit } from "react-icons/md";
+import { GrView } from "react-icons/gr";
 
 const Farmers = () => {
-  return <div>Farmers</div>;
+  return (
+    <div>
+      <div className="m-10">
+        <input
+          className="w-1/2 rounded-lg"
+          type="text"
+          name="search"
+          id=""
+          placeholder="Search"
+        />
+        <div className="mt-10">
+          <Button>Add new farmer</Button>
+        </div>
+        <div className="overflow-x-auto">
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>Picture</Table.HeadCell>
+              <Table.HeadCell>First Name</Table.HeadCell>
+              <Table.HeadCell>Last Name</Table.HeadCell>
+              <Table.HeadCell>District</Table.HeadCell>
+              <Table.HeadCell>Address</Table.HeadCell>
+              <Table.HeadCell>Phone</Table.HeadCell>
+              <Table.HeadCell>Primary Farm</Table.HeadCell>
+              <Table.HeadCell>Crop</Table.HeadCell>
+              <Table.HeadCell>Farm size(acres)</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {farmersData.map((farmer) => (
+                <Table.Row
+                  key={farmer.id}
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    <img
+                      src={farmer.image}
+                      alt="image"
+                      className="rounded-full w-10 h-10"
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{farmer.firstName}</Table.Cell>
+                  <Table.Cell>{farmer.lastName}</Table.Cell>
+                  <Table.Cell>{farmer.district}</Table.Cell>
+                  <Table.Cell>{farmer.address}</Table.Cell>
+                  <Table.Cell>{farmer.phone}</Table.Cell>
+                  <Table.Cell>{farmer.primaryFarm}</Table.Cell>
+                  <Table.Cell>{farmer.crop}</Table.Cell>
+                  <Table.Cell>{farmer.farmSize}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Farmers;
