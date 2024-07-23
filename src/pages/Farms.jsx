f4dffe1a59c3a854f12c07a2b0e87702bd21fdc0
Emmanuel <e.fkumah@gmail.com>
2024-07-23 23:59:47 +0000

    Start add farms to farm page

--- src/pages/Farms.jsx
+++ src/pages/Farms.jsx
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "flowbite-react";
 
 import { AddFarm, FarmsList, Search } from "../components";
 import { useFarmersContext } from "../context/FarmersProvider";
 const Farms = () => {
   const [openFarmForm, setOpenFarmForm] = useState(false);
-  const { farmState } = useFarmersContext();
+  const { state } = useFarmersContext();
 
-  console.log(farmState);
+  useEffect(() => {
+    // filterFarm();
+
+    return () => {};
+  }, []);
+
+  console.log(state.farmers);
+  // const filterFarm = (state) => {
+  //   const result = state.farmers.map((farmer) => farmer.primaryFarm);
+  //   console.log(result);
+  // };
   return (
     <div className="m-10">
       <Search />
-      <div className="my-10">
+      {/* <div className="my-10">
         <Button onClick={() => setOpenFarmForm(true)}>Add new farm</Button>
       </div>
       <AddFarm openFarmForm={openFarmForm} setOpenFarmForm={setOpenFarmForm} />
       <section>
         <FarmsList />
-      </section>
+      </section> */}
     </div>
   );
 };
