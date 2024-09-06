729581b40969b7de17691618b49005c7f753ff54
Emmanuel <e.fkumah@gmail.com>
2024-09-06 12:19:14 +0000

    Display age of farmers

--- src/components/Farmers/FarmerForm.jsx
+++ src/components/Farmers/FarmerForm.jsx
@@ -52,8 +52,9 @@ const FarmerForm = ({ farmer }) => {
   }, [districtId]);
 
   const handleDateChange = (date) => {
+    console.log(date);
     const formattedDate = date.toISOString().split("T")[0]; // Formats to "YYYY-MM-DD"
-    // console.log(formattedDate);
+    console.log(formattedDate);
     setDateOfBirth(formattedDate);
     //setBirthDate({ ...birthDate, dateOfBirth: date });
   };
@@ -112,12 +113,10 @@ const FarmerForm = ({ farmer }) => {
 
   const handleRegionChange = (e) => {
     const id = e.target.value;
-    console.log(id);
     setRegionId(id);
   };
   const handleDistrictChange = (e) => {
     const id = e.target.value;
-    console.log(id);
     setDistrictId(id);
   };
 
@@ -294,7 +293,9 @@ const FarmerForm = ({ farmer }) => {
                       <Datepicker
                         name="dateOfBirth"
                         maxDate={new Date(2010, 1, 30)}
-                        // onSelectedDateChanged={(date) => handleDateChange(date)}
+                        value={dateOfBirth}
+                        dateFormat="yyyy-MM-dd"
+                        onSelectedDateChanged={(date) => handleDateChange(date)}
                       />
                     )}
                   </div>
@@ -467,12 +468,12 @@ export const action = async ({ request }) => {
     gender: data.get("gender"),
     homeAddress: data.get("homeAddress"),
     phone: data.get("phone"),
-    dateOfBirth: "2020-09-09",
+    dateOfBirth: data.get("dateOfBirth"),
     communityId: data.get("communityId"),
     farmerType: data.get("farmerType"),
     cropType: data.get("cropType"),
   };
-  console.log(submission);
+  // console.log(submission);
 
   try {
     const response = await axios.post(
--- src/pages/AddFarmer.jsx
+++ src/pages/AddFarmer.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { FarmerForm } from "../components";
-// import { redirect, json } from "react-router-dom";
-// import axios from "axios";
-// import { getAuthToken } from "../utils/auth";
 
 const AddFarmer = () => {
   return (
@@ -15,42 +12,3 @@ const AddFarmer = () => {
 };
 
 export default AddFarmer;
-
-// export const action = async ({ request }) => {
-//   const data = await request.formData();
-//   const token = getAuthToken();
-
-//   let submission = {
-//     firstName: data.get("firstName"),
-//     lastName: data.get("lastName"),
-//     gender: data.get("gender"),
-//     homeAddress: data.get("homeAddress"),
-//     phone: data.get("phone"),
-//     dateOfBirth: "1988-09-05",
-//     communityId: data.get("communityId"),
-//     farmerType: data.get("farmerType"),
-//     cropType: data.get("cropType"),
-//   };
-//   //console.log(submission);
-
-//   const response = await axios.post(
-//     "https://dev.bjlfarmersmarket.net/farmer",
-//     submission,
-//     {
-//       headers: {
-//         "X-Origin": "WEB",
-//         Authorization: `Bearer ${token}`,
-//       },
-//     }
-//   );
-
-//   console.log("Response from server:", response.data);
-//   if (response.data) {
-//     console.log("data saved successfully");
-//     return redirect("/app/farmers");
-//   }
-
-//   if (response.status !== 200) {
-//     return response;
-//   }
-// };
