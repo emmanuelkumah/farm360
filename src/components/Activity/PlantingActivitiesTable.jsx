c7a151ddfd7373284922e9bfa7b16ab6f0e76ea1
Emmanuel <e.fkumah@gmail.com>
2024-09-18 10:45:41 +0000

    Feat: add loading spinner to planting actitvities table

--- src/components/Activity/PlantingActivitiesTable.jsx
+++ src/components/Activity/PlantingActivitiesTable.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Table, Pagination } from "flowbite-react";
+import { Table, Pagination, Spinner } from "flowbite-react";
 import BackButton from "../BackButton";
+import { useNavigation } from "react-router-dom";
 
 const PlantingActivitiesTable = ({ data }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -11,6 +12,8 @@ const PlantingActivitiesTable = ({ data }) => {
   const lastItemIndex = currentPage * itemsPerPage;
   const firstItemIndex = lastItemIndex - itemsPerPage;
   const currentPlantingData = data.slice(firstItemIndex, lastItemIndex);
+
+  const navigation = useNavigation();
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
@@ -34,41 +37,48 @@ const PlantingActivitiesTable = ({ data }) => {
           />
         </div>
       </div>
-      <Table striped>
-        <Table.Head>
-          <Table.HeadCell>Farm name</Table.HeadCell>
-          <Table.HeadCell>Crop</Table.HeadCell>
-          <Table.HeadCell>Landsize convered</Table.HeadCell>
-          <Table.HeadCell>kilos Planted</Table.HeadCell>
-          <Table.HeadCell>Supervisor</Table.HeadCell>
-          <Table.HeadCell>Contact</Table.HeadCell>
-          <Table.HeadCell>Certificate</Table.HeadCell>
-        </Table.Head>
-        <Table.Body className="divide-y">
-          {currentPlantingData
-            .filter((item) => {
-              return search.toLowerCase() === ""
-                ? item
-                : item.farm.toLowerCase().includes(search);
-            })
-            .map((item) => (
-              <Table.Row
-                className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                key={item.id}
-              >
-                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                  {item.farm}
-                </Table.Cell>
-                <Table.Cell>{item.cropName}</Table.Cell>
-                <Table.Cell>{item.landSizeCovered}</Table.Cell>
-                <Table.Cell>{item.kilosPlanted}</Table.Cell>
-                <Table.Cell>{item.supervisorName}</Table.Cell>
-                <Table.Cell>{item.supervisorContact}</Table.Cell>
-                <Table.Cell>{item.supervisorQualification}</Table.Cell>
-              </Table.Row>
-            ))}
-        </Table.Body>
-      </Table>
+      {navigation.state === "loading" ? (
+        <div className="text-center">
+          <Spinner aria-label="Center-aligned spinner example" size="xl" />
+        </div>
+      ) : (
+        <Table striped>
+          <Table.Head>
+            <Table.HeadCell>Farm name</Table.HeadCell>
+            <Table.HeadCell>Crop</Table.HeadCell>
+            <Table.HeadCell>Landsize convered</Table.HeadCell>
+            <Table.HeadCell>kilos Planted</Table.HeadCell>
+            <Table.HeadCell>Supervisor</Table.HeadCell>
+            <Table.HeadCell>Contact</Table.HeadCell>
+            <Table.HeadCell>Certificate</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {currentPlantingData
+              .filter((item) => {
+                return search.toLowerCase() === ""
+                  ? item
+                  : item.farm.toLowerCase().includes(search);
+              })
+              .map((item) => (
+                <Table.Row
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                  key={item.id}
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {item.farm}
+                  </Table.Cell>
+                  <Table.Cell>{item.cropName}</Table.Cell>
+                  <Table.Cell>{item.landSizeCovered}</Table.Cell>
+                  <Table.Cell>{item.kilosPlanted}</Table.Cell>
+                  <Table.Cell>{item.supervisorName}</Table.Cell>
+                  <Table.Cell>{item.supervisorContact}</Table.Cell>
+                  <Table.Cell>{item.supervisorQualification}</Table.Cell>
+                </Table.Row>
+              ))}
+          </Table.Body>
+        </Table>
+      )}
+
       <div className="flex overflow-x-auto mt-10 sm:justify-center">
         <Pagination
           layout="pagination"
