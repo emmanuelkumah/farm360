b4187a8df1e6fae8658a25dfe61b682197e1ea95
Emmanuel <e.fkumah@gmail.com>
2024-10-02 14:03:49 +0000

    Filter by farmer by firstname

--- src/components/Farmers/FarmersList.jsx
+++ src/components/Farmers/FarmersList.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Table, Button, Pagination } from "flowbite-react";
+import { Table, Button, Spinner } from "flowbite-react";
 import { MdDelete, MdEdit } from "react-icons/md";
 import { LuEye } from "react-icons/lu";
 import { FaUser } from "react-icons/fa6";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { axiosbaseURL } from "../../api/axios";
 
 const FarmersList = () => {
@@ -12,35 +12,34 @@ const FarmersList = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const [totalElements, setTotalElements] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [itemsPerPage, setItemesPerPage] = useState(20);
   const [editFarmer, setEditFarmer] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [search, setSearch] = useState("");
 
-  const farmersData = useLoaderData();
-
-  console.log("loaded farmers", farmers);
-  const totalFarmers = farmersData.length;
-  const lastItemIndex = currentPage * itemsPerPage;
-  const firstItemIndex = lastItemIndex - itemsPerPage;
-  const currentFarmersData = farmersData.slice(firstItemIndex, lastItemIndex);
+  // const totalFarmers = farmersData.length;
+  // const lastItemIndex = currentPage * itemsPerPage;
+  // const firstItemIndex = lastItemIndex - itemsPerPage;
+  // const currentFarmersData = farmersData.slice(firstItemIndex, lastItemIndex);
 
   useEffect(() => {
     fetchFarmers(currentPage);
   }, [currentPage]);
 
   const fetchFarmers = async (page) => {
+    setIsLoading(true);
     try {
       const response = await axiosbaseURL.get(
-        `/farmers-paged?page=${page}&size=${20}&sort=dateCreated,desc`
+        `/farmers-paged?&page=${page}&size=${20}&sort=dateCreated,desc`
       );
-      console.log("response", response);
       setFarmers(response.data.data);
       setTotalPages(response.data.totalPages);
       setTotalElements(response.data.totalElements);
     } catch (error) {
       console.error("error fetching farmer", error.response);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -50,7 +49,6 @@ const FarmersList = () => {
   const handleSearch = (e) => {
     setSearch(e.target.value);
   };
-  const onPageChange = (page) => setCurrentPage(page);
 
   const handleDeleteFarmer = (id) => {
     dispatch({
@@ -82,74 +80,89 @@ const FarmersList = () => {
         />
       </div>
       <div className="overflow-x-auto">
-        <Table className="w-full" hoverable>
-          <Table.Head>
-            <Table.HeadCell>Picture</Table.HeadCell>
-            <Table.HeadCell>First Name</Table.HeadCell>
-            <Table.HeadCell>Last Name</Table.HeadCell>
-            <Table.HeadCell>Age</Table.HeadCell>
-            <Table.HeadCell>Contact</Table.HeadCell>
-            <Table.HeadCell>Region</Table.HeadCell>
-            <Table.HeadCell>District</Table.HeadCell>
-            <Table.HeadCell>Community</Table.HeadCell>
-            <Table.HeadCell>Group</Table.HeadCell>
-            <Table.HeadCell>Actions</Table.HeadCell>
-          </Table.Head>
-          <Table.Body className="divide-y">
-            {farmers.map((farmer) => (
-              <Table.Row
-                key={farmer.id}
-                className="bg-white dark:border-gray-700 dark:bg-gray-800"
-              >
-                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                  {farmer.imageUrl === null ? (
-                    <FaUser />
-                  ) : (
-                    <img
-                      src={farmer.imageUrl}
-                      alt="farmer picture"
-                      className="rounded-full w-10 h-10"
-                    />
-                  )}
-                </Table.Cell>
-                <Table.Cell>{farmer.firstName}</Table.Cell>
-                <Table.Cell>{farmer.lastName}</Table.Cell>
-                <Table.Cell>{farmer.age}</Table.Cell>
-                <Table.Cell>{farmer.phone}</Table.Cell>
-                <Table.Cell>{farmer.community.region}</Table.Cell>
+        {isLoading ? (
+          <Spinner aria-label="Extra large spinner example" size="xl" />
+        ) : (
+          <Table className="w-full" hoverable>
+            <Table.Head>
+              <Table.HeadCell>Picture</Table.HeadCell>
+              <Table.HeadCell>First Name</Table.HeadCell>
+              <Table.HeadCell>Last Name</Table.HeadCell>
+              <Table.HeadCell>Age</Table.HeadCell>
+              <Table.HeadCell>Contact</Table.HeadCell>
+              <Table.HeadCell>Region</Table.HeadCell>
+              <Table.HeadCell>District</Table.HeadCell>
+              <Table.HeadCell>Community</Table.HeadCell>
+              <Table.HeadCell>Group</Table.HeadCell>
+              <Table.HeadCell>Actions</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {farmers
+                .filter((farmer) => {
+                  return search.toLowerCase() === ""
+                    ? farmer
+                    : farmer.firstName.toLowerCase().includes(search);
+                })
+                .map((farmer) => (
+                  <Table.Row
+                    key={farmer.id}
+                    className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                  >
+                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      {farmer.imageUrl === null ? (
+                        <FaUser />
+                      ) : (
+                        <img
+                          src={farmer.imageUrl}
+                          alt="farmer picture"
+                          className="rounded-full w-10 h-10"
+                        />
+                      )}
+                    </Table.Cell>
+                    <Table.Cell>{farmer.firstName}</Table.Cell>
+                    <Table.Cell>{farmer.lastName}</Table.Cell>
+                    <Table.Cell>{farmer.age}</Table.Cell>
+                    <Table.Cell>{farmer.phone}</Table.Cell>
+                    <Table.Cell>{farmer.community.region}</Table.Cell>
 
-                <Table.Cell>{farmer.community.district}</Table.Cell>
-                <Table.Cell>{farmer.community.name}</Table.Cell>
-                <Table.Cell>{farmer.group?.name}</Table.Cell>
+                    <Table.Cell>{farmer.community.district}</Table.Cell>
+                    <Table.Cell>{farmer.community.name}</Table.Cell>
+                    <Table.Cell>{farmer.group?.name}</Table.Cell>
 
-                <Table.Cell>
-                  <div className="flex gap-5">
-                    <Link to={`${farmer.id}`}>
-                      <LuEye className="text-xl hover:text-primary cursor-pointer" />
-                    </Link>
-                    <Link to={`${farmer.id}/edit`}>
-                      <MdEdit className="text-xl hover:text-teal-500 cursor-pointer" />
-                    </Link>
-                  </div>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+                    <Table.Cell>
+                      <div className="flex gap-5">
+                        <Link to={`${farmer.id}`}>
+                          <LuEye className="text-xl hover:text-primary cursor-pointer" />
+                        </Link>
+                        <Link to={`${farmer.id}/edit`}>
+                          <MdEdit className="text-xl hover:text-teal-500 cursor-pointer" />
+                        </Link>
+                      </div>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+            </Table.Body>
+          </Table>
+        )}
       </div>
       <div>
+        <div className="flex justify-center gap-4">
+          <Button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 0}
+          >
+            Previous
+          </Button>
+          <Button onClick={() => handlePageChange(currentPage + 1)}>
+            Next
+          </Button>
+        </div>
         <p>
           Showing page {currentPage} of {totalPages} (Total: {totalElements}{" "}
           farmers)
         </p>
       </div>
-      <Button
-        onClick={() => handlePageChange(currentPage - 1)}
-        disabled={currentPage === 0}
-      >
-        Previous
-      </Button>
-      <Button onClick={() => handlePageChange(currentPage + 1)}>Next</Button>
+
       {/* <div className="flex overflow-x-auto mt-10 sm:justify-center">
         <Pagination
           layout="pagination"
